274b81786a97e64dc60c8819798740b513f21473
TURAB1 <turabayochryso98@gmail.com>
2024-08-14 15:49:57 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header-section">
+    <h1>Company Ltd</h1>
+    <div class="left-header-section">
+      <h1 @click="loginHandle">Login</h1>
+      <h1>About us</h1>
+      <h1>Contact us</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  methods:{
+    loginHandle(){
+        console.log("login clicked")
+    }
+  }
+};
+</script>
+
+<style scoped>
+.header-section {
+  background-color: blue;
+  display: flex;
+  justify-content: space-around;
+  cursor: context-menu;
+}
+.left-header-section{
+  
+   display: flex; 
+   justify-content: space-evenly;
+   width: 30%;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>modal</h2>
+</template>
+
+<script>
+export default {
+    name:"Modal"
+}
+</script>
+
+<style scoped>
+ #modal{
+    color:aqua
+ }
+</style>
